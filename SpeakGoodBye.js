7d863267c3b71d11c81ec32db1f2c9462c9ae2f0
Saurav Kumar <71901100+saurav-kumar11@users.noreply.github.com>
2020-10-02 20:21:45 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -5,7 +5,7 @@
 // See Lecture 52, part 2
 (function(window){
 var byeSpeaker = {};
-var speakWord = "Good Bye";
+var speakWord = "Good Bye brother";
 byeSpeaker.speak=function (name) {
   console.log(speakWord + " " + name);
 }
